5f2ce0702c781090f679a97b46ccd29088c4a862
Ricks-Pennington <k.a.ricks-pennington@accenturefederal.com>
2020-06-05 10:24:26 -0500

    commiting

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -20,8 +20,6 @@ export class AuthComponent implements OnInit {
     this.isLogin = !this.isLogin;
   }
 
-  //neeed to commit
-
   onSubmit(form: NgForm) {
     console.log(form.value);
     const email = form.value.email;
